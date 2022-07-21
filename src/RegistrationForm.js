d531a80f102a8333aade1bccf78b0647c348a26b
srinivasbanavath <srinivasnaik77022@gmail.com>
2022-07-21 19:18:56 +0530

    post saving a user working

--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -1,39 +1,56 @@
 import { React, useEffect, useState } from "react";
 
 export const RegistrationForm = () => {
-  const [data, setData] = useState({
-});
+  const data = {
+    firstName: "",
+    lastName: "",
+    mobileNumber: "",
+    age: ""
+  };
+  const [dataLoading, setDataLoading] = useState(true);
 
-  useEffect(() => {
-    fetch("http://localhost:8080/api/allUser", {method: "GET"}).then(response => response.json).
-    then(result => setData(result));
-  }, []) 
+//   useEffect(() => {
+//     setDataLoading(true);
+//     fetch("http://localhost:8080/api/allUsers", {method: "GET"}).then(response => response.json).
+//     then(result => {
+//         setData(result);
+//         setDataLoading(false)
+//     });
+//   }, []) 
 
   const handleSubmit = () => {
-    var firstName = document.getElementById("firstName").value;
-    var lastName = document.getElementById("lastName").value;
-    var age = document.getElementById("age").value;
-    var phoneNumber = document.getElementById("phoneNumber").value;
 
+    data.firstName = document.getElementById("firstName").value;
+    data.lastName = document.getElementById("lastName").value;
+    data.age = document.getElementById("age").value;
+    data.mobileNumber = document.getElementById("phoneNumber").value;
+    
+    fetch("http://localhost:8080/api/users", {method: "POST",
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(data)})
   };
 
+ 
   return (
     <>
+    {/* {dataLoading ? <h1> loading...</h1> : ""} */}
       <form id="registrationForm">
         <ul>
-          <label for="firstName">First Name: </label>
+          <label htmlFor="firstName" >First Name: </label>
           <input placeholder="First name" id="firstName" name="firstName" />
         </ul>
         <ul>
-          <label for="lastName">Last Name: </label>
+          <label htmlFor="lastName">Last Name: </label>
           <input placeholder="Last name" id="lastName" name="lastName" />
         </ul>
         <ul>
-          <label for="age">Age: </label>
+          <label htmlFor="age">Age: </label>
           <input placeholder="Age" id="age" name="age" />
         </ul>
         <ul>
-          <label for="phoneNumber">Phone Number: </label>
+          <label htmlFor="phoneNumber">Phone Number: </label>
           <input placeholder="phoneNumber" id="phoneNumber" name="phoneNumber" />
         </ul>
         <button id="submit" onClick={handleSubmit}>
