5d8e64f2bb169b7d22363dd82c934fce7304a8e2
srinivasbanavath <srinivasnaik77022@gmail.com>
2022-07-21 17:21:48 +0530

    tillRegistration Form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import { Routes, Route} from "react-router-dom";
+import { useState } from 'react';
+import { Routes, Route, Link} from "react-router-dom";
 import {RegistrationForm} from './RegistrationForm';
 import {Users} from './Users';
 
 function App() {
+  const [startsStatus, setStartStatus] = useState("Registration Form");
+  const [status, setStatus] = useState("Y");
+
+  const handleStatus = () => {
+    if(startsStatus == "Registration Form" & status == "Y"){
+        setStartStatus("");
+    }
+}
+
   return (
+    <>
+    <Link to={"/registrationForm"} onClick={handleStatus} >{startsStatus}</Link>
     <Routes>
-        <Route path='/' element={<RegistrationForm />}/>
+        <Route path='/registrationForm' element={<RegistrationForm />}/>
         <Route path='/users' element={<Users/>}/>
     </Routes>
+    </>
   );
 }
 
