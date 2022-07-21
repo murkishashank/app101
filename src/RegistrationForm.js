784a55821ae984d3535940617a48ac4117cc8eb9
Shashank.M <sunny286.ss@gmail.com>
2022-07-21 16:09:12 +0530

    first commit

---
+++ src/RegistrationForm.js
@@ -0,0 +1,9 @@
+import {React} from 'react';
+
+export const RegistrationForm = () =>{ 
+    return(
+        <>
+        <h1>Welcome!!!</h1>
+        </>
+    );
+}
---
+++ src/Users.js
@@ -0,0 +1,8 @@
+import {React} from 'react';
+
+export const Users = () =>{ 
+    console.log("Hello");
+    return(
+        <h1>Welcome!!!</h1>
+    );
+}
