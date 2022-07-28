af99b7117e406aec2a0623d30d247edbea029d70
Saikrishnabirudugadda <1437bsk@gmail.com>
2022-07-28 16:59:54 +0530

    Implementd nav bar design.

--- src/NavBar.js
+++ src/NavBar.js
@@ -6,22 +6,26 @@ import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar className="navbar navbar-dark bg-primary">
       <Container>
         <Navbar.Brand as={Link} to="/home">
-          App
+          <img src={"../Tecnics.png"} height="25"></img>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" style={{ float: "right" }}>
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/home">
-              Home
+              <b>
+                <h5> Home</h5>
+              </b>
             </Nav.Link>
             {/* <Nav.Link as={Link} to="/leave">
               Leave
             </Nav.Link> */}
             <Nav.Link as={Link} to="/profile">
-              profile
+              <b>
+                <h5>profile</h5>
+              </b>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
