f3ff017366b0791891ef927752d574d6f72cf61d
Saikrishnabirudugadda <1437bsk@gmail.com>
2022-08-01 20:17:49 +0530

    Removed cmmented code.

--- src/pages/WorkStatus/WorkStatus.js
+++ src/pages/WorkStatus/WorkStatus.js
@@ -41,50 +41,6 @@ export const WorkStatus = () => {
     setRowData(rowDataClone);
   };
 
-  // const handleFilterChange = (value) => {
-  //   setFilter(value);
-  //   if (value === "No filter") {
-  //     setFilterData([]);
-  //   }
-  // };
-
-  // const handleFilterValueChange = (event) => {
-  //   console.log("name2", event);
-  //   if (filter === "Task status") {
-  //     const filteredRowData = rowData.filter((row) => row.taskStatus === event);
-  //     setFilterData(filteredRowData);
-  //   }
-  // };
-
-  // const getFilterOptions = (filterValue) => {
-  //   if (filterValue === "Task status") {
-  //     return (
-  //       <Form.Select
-  //         onChange={(event) => {
-  //           handleFilterValueChange(event.target.value);
-  //         }}
-  //         name="filter type"
-  //         defaultValue={filter}
-  //       >
-  //         {taskStatusOptions.map((option) => {
-  //           return <option>{option}</option>;
-  //         })}
-  //       </Form.Select>
-  //     );
-  //   }
-  //   if (filterValue === "Date") {
-  //     return (
-  //       <Form.Control
-  //         type="date"
-  //         name="filterDate"
-  //         placeholder="From Date"
-  //         onChange={handleFilterValueChange}
-  //         defaultValue={filter}
-  //         autoFocus
-  //       />
-  //     );
-  //   }
-  // };
   const handleSave = () => {
     const payload = getSavePayload(rowData);
     PostWorkStatus(payload);
@@ -99,23 +55,6 @@ export const WorkStatus = () => {
           <Button variant="secondary" onClick={handleSave}>
             Save
           </Button>
-          {/* <Form.Label>Filter by: </Form.Label>
-          <div
-            style={{ width: "180px", marginLeft: "120px", marginTop: "-40px" }}
-          >
-            <Form.Select
-              onChange={(event) => {
-                handleFilterChange(event.target.value);
-              }}
-              name="filter type"
-              value={filter}
-            >
-              <option>No filter</option>
-              <option>Date</option>
-              <option>Task status</option>
-            </Form.Select>
-            {getFilterOptions(filter)}
-          </div> */}
         </div>
         <DataGrid
           rows={rowData}
