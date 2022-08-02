4862c9a38ac68995b6051bd0e283c39f6f1bf2d2
srinivasbanavath <srinivasnaik77022@gmail.com>
2022-08-02 11:05:38 +0530

    till adding approved date and successfully loading data without reload

--- src/pages/CommonColumns.js
+++ src/pages/CommonColumns.js
@@ -3,7 +3,7 @@ export const commonColumns = [
   { field: "userId", headerName: "User Id" },
   { field: "id", headerName: "requestId" },
   { field: "fromDate", headerName: "fromDate" },
-  { field: "toDate", headerName: "Age" },
+  { field: "toDate", headerName: "toDate" },
   { field: "appliedDate", headerName: "Applied Date" },
   { field: "approvedDate", headerName: "Approved Date" },
   { field: "reason", headerName: "Reason" },
