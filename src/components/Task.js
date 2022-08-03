0d603c1d272d7ce8325f3601dfc6516f8c0d328c
Your Name <mounika-bhavani>
2022-08-03 13:12:12 +0530

    resolved issues

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useState } from "react";
-import { TaskCard } from "../components/TaskCard";
 import { NavBar } from "../components/NavBar";
 import "../css/CommonStyling.css";
 import Button from "react-bootstrap/Button";
