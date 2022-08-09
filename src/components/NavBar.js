606f9e18fd34c9e5f6b4f991e7514c2a17010c22
Saikrishnabirudugadda <1437bsk@gmail.com>
2022-08-09 12:40:32 +0530

    Changed path varibale names

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -26,7 +26,7 @@ export const NavBar = () => {
                     <h5>Home</h5>
                   </b>
                 </Nav.Link>
-                <Nav.Link as={Link} to="/workStatus">
+                <Nav.Link as={Link} to="/work-status">
                   <b>
                     <h5>Work Status</h5>
                   </b>
@@ -57,7 +57,7 @@ export const NavBar = () => {
                     <h5>Leaves data</h5>
                   </b>
                 </Nav.Link>
-                <Nav.Link as={Link} to="/empDetails">
+                <Nav.Link as={Link} to="/emp-details">
                   <b>
                     <h5>Manage Employee Details</h5>
                   </b>
--- src/routes/App.js
+++ src/routes/App.js
@@ -57,7 +57,7 @@ function App() {
 
         <Route path="/home" element={<Home />} />
         <Route path="/profile" element={<Profile userData={userData} />} />
-        <Route path="/workstatus" element={<WorkStatus />} />
+        <Route path="/work-status" element={<WorkStatus />} />
 
         {/* Admin Routes */}
 
@@ -75,18 +75,18 @@ function App() {
           element={<Admin prevoiusAppl={handlePrevoiusAppl} />}
         />
         {/* <Route path="/editProfile" element={<EditProfile userData={userData} />} /> */}
-        <Route path="/workStatus" element={<WorkStatus />} />
+        <Route path="/work-status" element={<WorkStatus />} />
         <Route path="/taskForm" element={<Task />} />
 
         {/* HR Routes */}
         <Route
-          path="/empDetails"
+          path="/emp-details"
           element={
             <ManageEmpDetails setEditEmpDetails={handleEmpEditDetails} />
           }
         />
         <Route
-          path="/editEmpDetails"
+          path="/edit-emp-details"
           element={<Profile editEmp={empDetailsEdit} />}
         />
         <Route path="/hr/leavesdata" element={<HumanResource />} />
