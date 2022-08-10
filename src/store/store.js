d8229c4f84b0676eb808322e9bd5c96886f83ab0
srinivasbanavath <srinivasnaik77022@gmail.com>
2022-08-10 11:43:38 +0530

    added filter in hr

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import workStatusUserReducer from "../pages/WorkStatus/slice/actions";
-import loginFormReducer from "../pages/LoginForm/slice/action";
+import loginFormReducer from "../pages/LoginForm/Slice/action";
 import manageEmpReducer from "../pages/ManageEmpDetails/slice/action";
 
 export const store = configureStore({
