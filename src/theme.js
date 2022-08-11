5bcac583595dd1b426115178625c143ff61a805c
niyatip25 <niyatip25@gmail.com>
2022-08-11 19:05:12 +0530

    Login Form Design Changes

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,14 @@ import { createTheme } from "@mui/material/styles";
 
 // A custom theme for this app
 const theme = createTheme({
-  palette: {},
+  palette: {
+    primary: {
+      main: "#eb0101",
+    },
+    secondary: {
+      main: "#98a27f",
+    },
+  },
 });
 
 export default theme;
